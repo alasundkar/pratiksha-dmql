0a2a4cbff1ea759a8c4ec5081fb281002e2647c0
Rupesh Alasundkar <ralasundkar@MBP-0034996.local>
2024-12-02 18:24:30 -0700

    update app.py to listen to all

--- app.py
+++ app.py
@@ -102,4 +102,4 @@ def index():
     return render_template('index.html', query=query, result=result, message=message)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=True)
